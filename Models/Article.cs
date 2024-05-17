c0ccb0c9456f181cad57c409c275ff3202cf86d3
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2024-05-18 00:22:23 +0200

    It's for test.

---
+++ Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Article
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Content { get; set; }
+	public string Category { get; set; }
+	public string Author { get; set; }
+	public DateTime PublishDate { get; set; }
+}
